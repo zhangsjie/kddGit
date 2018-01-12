b8de67b4c3dddb2d8a1dbbbac17fea2cd0f4b5f7
zhangsjie <852618587@qq.com>
2018-01-12 18:17:08 +0800

    new springboot

---
+++ src/main/java/hpe/clqr/demo/KddGitApplication.java
@@ -0,0 +1,12 @@
+package hpe.clqr.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KddGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KddGitApplication.class, args);
+	}
+}
